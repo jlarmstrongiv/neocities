a86d6b762b6a16c22b4a39eca4f9e0958338e13f
John Armstrong <jlarmst@g.clemson.edu>
2018-10-17 12:32:13 -0400

    fixed updateObject

--- packages/front-end/src/utilities/updateObject/updateObject.js
+++ packages/front-end/src/utilities/updateObject/updateObject.js
@@ -2,8 +2,8 @@ export const updateObject = (oldState, updatedProperties) => {
   let newState = { ...oldState, };
   for (let key in updatedProperties) {
     newState[key] = updatedProperties[key];
-    if (typeof updatedProperties[key] === 'object') {
-      newState[key] = updateObject(oldState[key], newState[key]);
+    if (updatedProperties[key] !== null && typeof updatedProperties[key] === 'object') {
+      newState[key] = updateObject((oldState[key] || {}), newState[key]);
     }
   }
   return newState;
